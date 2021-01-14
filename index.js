dd865d19bb8781825242389cc13b8149dad2aae3
Thijs Putman <thijs@sgraastra.net>
2021-01-14 15:57:47 +0100

    Rework into a continuous main loop (WIP)
    Instead of running once, the script now keeps running, fetching data as
    fast as it can. On receiving SIGINT (Ctrl+C) it (attempts to) gracefully
    terminate after finishing the current iteration. Failures should lead to
    a non-zero exit-code.
    
    Works, but still a bit rough around the edges...

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
+"use strict";
 
 const { createBluetooth } = require("node-ble");
+const { bluetooth, destroy } = createBluetooth();
 const MQTT = require("async-mqtt");
 const crypto = require("crypto");
 
@@ -11,101 +13,157 @@ const key = Buffer.from([
   0x86, 0xf4, 0x02, 0x60, 0x81, 0x6f, 0x9a, 0x0b,
   0xa7, 0xf1, 0x06, 0x61, 0x9a, 0xb8, 0x72, 0x88
 ]);
-const key_algo = "aes-256-ecb";
+const keyAlgo = "aes-256-ecb";
+
+const getDeviceChrs = (device) => {
+  let resolved = false;
+  return new Promise((resolve, reject) => {
+    // 5 Second timeout on getting device characteristics
+    setTimeout(() => {
+      if (!resolved) {
+        reject(new Error("Timeout"));
+      }
+    }, 5000);
+
+    let txChr;
+    let rxChr;
+
+    (async () => {
+      try {
+        const gattServer = await device.gatt();
+
+        const tx = await gattServer.getPrimaryService(
+          "0000ffe5-0000-1000-8000-00805f9b34fb"
+        );
+        txChr = await tx.getCharacteristic(
+          "0000ffe9-0000-1000-8000-00805f9b34fb"
+        );
+        const rx = await gattServer.getPrimaryService(
+          "0000ffe0-0000-1000-8000-00805f9b34fb"
+        );
+        rxChr = await rx.getCharacteristic(
+          "0000ffe4-0000-1000-8000-00805f9b34fb"
+        );
+      } catch (error) {
+        reject(error);
+      }
+
+      resolved = true;
+      resolve({ txChr: txChr, rxChr: rxChr });
+    })();
+  });
+};
+
+const receiveBuffer = (rxChr) => {
+  const response = [];
+  let length = 0;
+  let resolved = false;
+  return new Promise((resolve, reject) => {
+    // 5 Second timeout on receiving data
+    setTimeout(() => {
+      if (!resolved) {
+        reject(new Error("Timeout"));
+      }
+    }, 5000);
+    /**
+     * XXX: Not sure how nice of an approach this is...
+     *
+     * We need to remove the listener from our previous invocation(s) to prevent
+     * them from lingering around (receiving data, but not acting upon them as
+     * their containing promise already got resolved) and creating (what I
+     * assume will be) a significant memory leak.
+     * Could rewrite to lift the listener out of this function, but that would
+     * needlessly complicate the logic?
+     */
+    rxChr.removeAllListeners("valuechanged");
+    rxChr.on("valuechanged", (buffer) => {
+      response.push(buffer);
+      length += buffer.length;
+      console.debug("Buffer length", length);
+      if (length === 192) {
+        resolved = true;
+        resolve(response);
+      } else if (length > 192) {
+        reject(new Error(`Buffer length ${length} exceeded 192`));
+      }
+    });
+  });
+};
+
+const sendMQTT = async (broker, messages) => {
+  const mqttClient = await MQTT.connectAsync(`tcp://${broker}`);
+
+  for (const [topic, value] of messages) {
+    console.info(topic, value);
+    await mqttClient.publish(topic, value.toString());
+  }
+
+  await mqttClient.end();
+};
 
 let data;
+let exitLoop = false;
+let exitCode = 0;
+
+process.on("SIGINT", () => {
+  exitLoop = true;
+});
 
 (async () => {
-  const { bluetooth, destroy } = createBluetooth();
-
   const adapter = await bluetooth.defaultAdapter();
-  console.debug("Is powered", await adapter.isPowered());
+  console.info("Is powered", await adapter.isPowered());
 
-  // It appears discovery is needed to (reliably) connect?
+  // It appears discovery is needed to (reliably) connect...
   if (!(await adapter.isDiscovering())) {
     await adapter.startDiscovery();
   }
 
   const device = await adapter.waitDevice(process.argv[2]);
   await device.connect();
-  console.debug("Connected to", await device.getName());
+  console.info("Connected to", await device.getName());
 
   try {
-    const gattServer = await device.gatt();
+    const { txChr, rxChr } = await getDeviceChrs(device);
 
-    const tx = await gattServer.getPrimaryService(
-      "0000ffe5-0000-1000-8000-00805f9b34fb"
-    );
-    const txChr = await tx.getCharacteristic(
-      "0000ffe9-0000-1000-8000-00805f9b34fb"
-    );
-    const rx = await gattServer.getPrimaryService(
-      "0000ffe0-0000-1000-8000-00805f9b34fb"
-    );
-    const rxChr = await rx.getCharacteristic(
-      "0000ffe4-0000-1000-8000-00805f9b34fb"
-    );
+    while (true) {
+      await rxChr.startNotifications();
+      await txChr.writeValue(Buffer.from("bgetva\r\n", "ascii"));
 
-    const receiveBuffer = () => {
-      let response = [];
-      let length = 0;
-      let resolved = false;
-      return new Promise((resolve, reject) => {
-        // Time out after 5 seconds if receive promise is not resolved
-        setTimeout(() => {
-          if (!resolved) {
-            reject("Timed out");
-          }
-        }, 5000);
-        rxChr.on("valuechanged", (buffer) => {
-          response.push(buffer);
-          length += buffer.length;
-          console.debug("Buffer length", length);
-          if (length >= 192) {
-            resolved = true;
-            resolve(response);
-          }
-        });
-      });
-    };
+      data = Buffer.concat(await receiveBuffer(rxChr));
+      console.info("Buffer", data);
 
-    await rxChr.startNotifications();
+      await rxChr.stopNotifications();
 
-    await txChr.writeValue(Buffer.from("bgetva\r\n", "ascii"));
+      const decipher = crypto.createDecipheriv(keyAlgo, key, "");
+      const decrypted = decipher.update(data);
+      const messages = [];
 
-    data = Buffer.concat(await receiveBuffer());
-    console.debug(data);
+      const voltageV = decrypted.readInt32LE(48) * 1e-4;
+      messages.push(["tc66c/voltage_V", voltageV]);
 
-    await rxChr.stopNotifications();
+      const currentA = decrypted.readInt32LE(52) * 1e-5;
+      messages.push(["tc66c/current_A", currentA]);
+
+      const powerW = decrypted.readInt32LE(56) * 1e-4;
+      messages.push(["tc66c/power_W", powerW]);
+
+      // TODO: Abort existing promise if it's still running when we get here again?
+
+      sendMQTT(process.argv[3], messages);
+
+      // TODO: Add (configurable) sleep
+
+      if (exitLoop) {
+        break;
+      }
+    }
   } catch (error) {
     console.error(error);
-
-    await device.disconnect();
-    destroy();
-
-    process.exit(1);
+    exitCode = 1;
   }
 
   await device.disconnect();
   destroy();
 
-  let decipher = crypto.createDecipheriv(key_algo, key, "");
-  let decrypted = decipher.update(data);
-
-  let voltageV = decrypted.readInt32LE(48) * 1e-4;
-  console.info("Voltage (V)", voltageV);
-
-  let currentA = decrypted.readInt32LE(52) * 1e-5;
-  console.info("Current (A)", currentA);
-
-  let powerW = decrypted.readInt32LE(56) * 1e-4;
-  console.info("Power (W)", powerW);
-
-  const mqttClient = await MQTT.connectAsync(`tcp://${process.argv[3]}`);
-
-  await mqttClient.publish("tc66c/volgate_V", voltageV.toString());
-  await mqttClient.publish("tc66c/current_A", currentA.toString());
-  await mqttClient.publish("tc66c/power_W", powerW.toString());
-
-  await mqttClient.end();
+  process.exit(exitCode);
 })();
